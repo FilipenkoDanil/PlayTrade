8f9216f3bd40ae57d6aa2da6177c305d5dcd3960
FilipenkoDanil <dan.filipenko@yandex.ru>
2025-02-28 21:33:50 +0200

    feat: Notify seller when a rating is submitted
    Adds functionality to send a message to the seller when a
    buyer submits a rating for a deal. This enhances communication
    and awareness of feedback.

--- app/Http/Controllers/API/RatingController.php
+++ app/Http/Controllers/API/RatingController.php
@@ -6,11 +6,18 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Rating\StoreRatingRequest;
 use App\Http\Requests\Rating\UpdateRatingRequest;
 use App\Http\Resources\RatingResource;
+use App\Models\Deal;
 use App\Models\Rating;
+use App\Services\ChatService;
+use App\Services\MessageService;
 use Illuminate\Support\Facades\Auth;
 
 class RatingController extends Controller
 {
+    public function __construct(private readonly ChatService $chatService, private readonly MessageService $messageService)
+    {
+
+    }
     /**
      * Display a listing of the resource.
      */
@@ -34,6 +41,10 @@ class RatingController extends Controller
                 'comment' => $request->comment,
             ]
         );
+
+        $deal = Deal::find($request->deal_id);
+        $chat = $this->chatService->createOrGetChat(Auth::id(), $deal->offer->seller->id);
+        $this->messageService->sendDealNotification($deal, $chat, 'rating');
 
         return new RatingResource($rating);
     }
